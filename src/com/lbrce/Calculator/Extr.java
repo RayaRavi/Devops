09667948f769136633ec92d308a13ff063c5ee86
RayaRavi <125902012+RayaRavi@users.noreply.github.com>
2024-11-22 14:58:00 +0530

    Extr.java

--- src/com/lbrce/Calculator/Extr.java
+++ src/com/lbrce/Calculator/Extr.java
@@ -6,10 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class Extr
  */
 @WebServlet("/login") // URL mapping for the servlet
-public class LoginServlet extends HttpServlet {
+public class Extr extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
